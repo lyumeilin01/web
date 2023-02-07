ed5bf5f04fb1bb538d53d45ef4b2ac915c3b9858
meilinlyu <meilin.lyu@mail.mcgill.ca>
2023-02-06 20:42:42 -0500

    added homepage

---
+++ src/pages/Project1.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Project1 = () => (
+    <div style={{ background: '#D0D0D5', minHeight: '100vh', color:'1B1B32' }}>
+      &emsp; &emsp; &emsp;
+      <h1 style={{ color: 'blue', textAlign: 'center', marginTop: '1rem' }}>Project 1</h1>
+      <p style={{ color: 'blue', marginTop: '2rem' }}> 
+ &nbsp; ^__^ &nbsp;&lt; Projects&gt;<br></br>
+        &nbsp;&nbsp;(oo)\____ <br></br>
+        &nbsp;(__)\ &emsp;&emsp;&nbsp;)\/\ <br></br>
+        &emsp;&emsp;||----w | <br></br>
+        &emsp;&emsp;||     &emsp;&emsp;||</p> 
+    </div>
+  );
+  
+  export default Project1;
